2e50ba7255f92eea947df4a89828f95e088c26d6
Glen <glen.84@gmail.com>
2025-07-14 15:52:05 +0200

    Placed new line before binary operators (#8445)

--- src/HotChocolate/ApolloFederation/src/ApolloFederation/Resolvers/ArgumentParser.cs
+++ src/HotChocolate/ApolloFederation/src/ApolloFederation/Resolvers/ArgumentParser.cs
@@ -38,8 +38,8 @@ internal static class ArgumentParser
             {
                 var current = path[i];
 
-                if (type is not IComplexTypeDefinition complexType ||
-                    !complexType.Fields.TryGetField(current, out var field))
+                if (type is not IComplexTypeDefinition complexType
+                    || !complexType.Fields.TryGetField(current, out var field))
                 {
                     break;
                 }
@@ -119,8 +119,8 @@ internal static class ArgumentParser
 
         if (required.Count == 2)
         {
-            return Matches(valueNode, required[0], 0) &&
-                Matches(valueNode, required[1], 0);
+            return Matches(valueNode, required[0], 0)
+                && Matches(valueNode, required[1], 0);
         }
 
         for (var i = 0; i < required.Count; i++)
